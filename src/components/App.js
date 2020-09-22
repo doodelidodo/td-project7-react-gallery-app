608c6e82a668bd135b01cb85bb891a0d316ea59f
doodelidodo <dominic.meyer@me.com>
2020-09-22 13:38:02 +0200

    api implementation

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
+import '../css/index.css';
+import apiKey from "../config";
+import axios from 'axios';
+export default class App extends Component {
 
-function App() {
-  return (
-    <div className="container">
-      hello world
-    </div>
-  );
+    constructor() {
+        super();
+        this.state = {
+            photos: [],
+            loading: true
+        };
+    }
+
+
+    componentDidMount() {
+        this.performSearch("cats");
+    }
+
+    performSearch = (query) => {
+        axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&format=json&nojsoncallback=1`)
+            .then(response => {
+                console.log(response);
+                this.setState({
+                    photos: response.data.photos.photo,
+                    loading: false
+                })
+            })
+            .catch(error => {
+                console.log('Error fetching and parsing data', error);
+            });
+    }
+
+    render() {
+        console.log(this.state.photos);
+        return (
+            <div className="container">
+                hello world
+            </div>
+        );
+    }
+
 }
-
-export default App;
